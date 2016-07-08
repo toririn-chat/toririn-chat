47818b3de6f2791a6145e8ad2c148388ad39b8f0
Masayuki Higashino <msyk@hgsn.info>
2016-07-08 20:55:50 +0900

    add a validater

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :messages
+  validate :name_cannot_be_blank
+
+ def name_cannot_be_blank
+   if !name.present? || name.strip.blank?
+     errors.add(:name, 'cannot be blank')
+   end
+ end
+
 end
