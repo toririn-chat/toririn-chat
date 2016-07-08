3740b6cbb06f474b4c01f42a0f64692e440f74b0
Masayuki Higashino <msyk@hgsn.info>
2016-07-08 17:31:28 +0900

    cleanup

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,28 +1,20 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update, :destroy]
 
-  # GET /rooms
-  # GET /rooms.json
   def index
     @rooms = Room.all.includes(:messages)
   end
 
-  # GET /rooms/1
-  # GET /rooms/1.json
   def show
   end
 
-  # GET /rooms/new
   def new
     @room = Room.new
   end
 
-  # GET /rooms/1/edit
   def edit
   end
 
-  # POST /rooms
-  # POST /rooms.json
   def create
     @room = Room.new(room_params)
 
@@ -37,8 +29,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /rooms/1
-  # PATCH/PUT /rooms/1.json
   def update
     respond_to do |format|
       if @room.update(room_params)
@@ -51,8 +41,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # DELETE /rooms/1
-  # DELETE /rooms/1.json
   def destroy
     @room.destroy
     respond_to do |format|
