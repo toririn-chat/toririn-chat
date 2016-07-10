38c83b81e9bfddbf7a1ae6b78bf2f5700bd368cd
Masayuki Higashino <msyk@hgsn.info>
2016-07-11 02:08:17 +0900

    add tests for users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,48 +1,21 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
   setup do
-    @user = users(:one)
+    @user = users(:user_0)
   end
 
-  test "should get index" do
-    get users_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_user_url
-    assert_response :success
-  end
-
-  test "should create user" do
+  test 'create user' do
     assert_difference('User.count') do
-      post users_url, params: { user: { name: @user.name } }
+      post users_url(format:'json'), {params:{user:{name:@user.name, icon:@user.icon}}}
     end
-
-    assert_redirected_to user_url(User.last)
-  end
-
-  test "should show user" do
-    get user_url(@user)
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_user_url(@user)
+  test 'update user' do
+    patch user_url(@user, format:'json'), {params:{user:{name:@user.name, icon:@user.icon}}}
     assert_response :success
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { name: @user.name } }
-    assert_redirected_to user_url(@user)
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user)
-    end
-
-    assert_redirected_to users_url
-  end
 end
