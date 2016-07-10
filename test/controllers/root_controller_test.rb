72109b7a170a55c9fa383085dd106bf6321fb206
Masayuki Higashino <msyk@hgsn.info>
2016-07-11 01:36:55 +0900

    cleanup

--- test/controllers/root_controller_test.rb
+++ test/controllers/root_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class RootControllerTest < ActionDispatch::IntegrationTest
 
-  test 'should get /' do
+  test 'get /' do
     get root_url
     assert_response :success
   end
 
-  test 'should get /about' do
+  test 'get /about' do
     get about_url
     assert_response :success
   end
