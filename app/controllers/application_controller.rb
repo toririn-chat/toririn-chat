a03b4616f6c32a8d54160d154744a4678a64893d
Masayuki Higashino <msyk@hgsn.info>
2017-08-21 00:45:30 +0900

    Remove un-used handlers of redirections for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
-
   # protect_from_forgery with: :exception
-
-  def after_sign_in_path_for(resource)
-    teacher_rooms_path
-  end
-
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
 end
