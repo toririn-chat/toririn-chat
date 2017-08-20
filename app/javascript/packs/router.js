ea677bbc714d145f1c393e0d3c71bbf53d6b6a31
Masayuki Higashino <msyk@hgsn.info>
2017-08-21 01:20:44 +0900

    Add signout function (close #24)

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -3,12 +3,17 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import Root from './components/Root'
+import Dashboard from './components/Dashboard'
 
 const routes = [
   {
     path: '/',
     name: 'root',
     component: Root
+  }, {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard
   }
 ]
 export default new VueRouter({routes, mode: 'history'})
