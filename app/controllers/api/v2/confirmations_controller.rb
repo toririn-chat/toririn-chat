2f7ad31b7f7c1f2b690cf4922d141fd30ce6c451
Masayuki Higashino <msyk@hgsn.info>
2017-08-21 00:45:49 +0900

    Cleanup

--- app/controllers/api/v2/confirmations_controller.rb
+++ app/controllers/api/v2/confirmations_controller.rb
@@ -1,17 +1,3 @@
 class Api::V2::ConfirmationsController < Devise::ConfirmationsController
-
   respond_to :json
-
-  # @see https://github.com/plataformatec/devise/blob/master/app/controllers/devise/confirmations_controller.rb
-  # GET /resource/confirmation?confirmation_token=abcdef
-  def show
-    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-    yield resource if block_given?
-    if resource.errors.empty?
-      respond_with(resource)
-    else
-      respond_with(resource.errors, status: :unprocessable_entity)
-    end
-  end
-
 end
