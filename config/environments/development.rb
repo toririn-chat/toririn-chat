3765694d00bce45e692b5e0f98265e90126fe94b
Masayuki Higashino <msyk@hgsn.info>
2017-08-22 13:19:17 +0900

    Support ENVs: RAILS_MAILER_URL_DOMAIN, RAILS_MAILER_URL_PROTOCOL, and RAILS_MAILER_URL_PORT (close #27)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,11 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {
+    protocol: ENV['RAILS_MAILER_URL_PROTOCOL'].presence || 'http',
+    host: ENV['RAILS_MAILER_URL_DOMAIN'].presence || 'localhost',
+    port: ENV['RAILS_MAILER_URL_PORT'].presence || '3000',
+  }
   config.action_mailer.delivery_method = :letter_opener_web
 
   # Print deprecation notices to the Rails logger.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,10 +62,12 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "toririn-chat_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {
+    protocol: ENV['RAILS_MAILER_URL_PROTOCOL'].presence,
+    host: ENV['RAILS_MAILER_URL_DOMAIN'].presence,
+    port: ENV['RAILS_MAILER_URL_PORT'].presence,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
