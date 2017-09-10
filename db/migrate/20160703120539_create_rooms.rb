d24a553f2e56b12aa5837728c8d516e9f4f1e6c4
Masayuki Higashino <msyk@hgsn.info>
2017-09-10 17:33:12 +0900

    Revert a model of the room

--- db/migrate/20160703120539_create_rooms.rb
+++ db/migrate/20160703120539_create_rooms.rb
@@ -2,7 +2,6 @@ class CreateRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :rooms do |t|
       t.string :name, null:false
-      t.integer :teacher_id, null:false, foreign_key:true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,10 @@
 ActiveRecord::Schema.define(version: 20170818030252) do
 
   create_table "messages", force: :cascade do |t|
-    t.integer  "room_id"
-    t.integer  "user_id"
-    t.text     "text"
-    t.text     "stamp"
+    t.integer "room_id"
+    t.integer "user_id"
+    t.text "text"
+    t.text "stamp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
@@ -24,43 +24,42 @@ ActiveRecord::Schema.define(version: 20170818030252) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.integer  "teacher_id", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "user_messages", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "message_id"
-    t.boolean  "read",       default: true, null: false
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.integer "user_id"
+    t.integer "message_id"
+    t.boolean "read", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["message_id"], name: "index_user_messages_on_message_id"
     t.index ["user_id"], name: "index_user_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "icon"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
     t.datetime "locked_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
