9452c14de0592e2618031da959b2465a1fe2c862
Masayuki Higashino <msyk@hgsn.info>
2018-09-18 13:18:22 +0900

    Update webpacker to 3.5 (close #60)

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,4 +1,5 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
 const environment = require('./environment')
-const custom = require('./custom')
-const merge = require('webpack-merge')
-module.exports = merge(environment.toWebpackConfig(), custom)
+
+module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,2 +1,9 @@
-const {environment} = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker')
+
+const vue = require('./loaders/vue')
+environment.loaders.append('vue', vue)
+
+const custom = require('./custom')
+environment.config.merge(custom)
+
 module.exports = environment
