ee394c879d88d5619f132ea207087e98ff4d2909
Masayuki Higashino <msyk@hgsn.info>
2018-09-18 14:08:20 +0900

    Fix missing loading of css

--- app/javascript/packs/components/App.vue
+++ app/javascript/packs/components/App.vue
@@ -3,14 +3,13 @@
 </router-view>
 </template>
 <script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import Vue from 'vue'
-import 'font-awesome-sass-loader'
 import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 export default {}
 </script>
 <style lang="scss">
-@import "../styles/valiables";
-@import 'bootstrap-vue/dist/bootstrap-vue.css';
-@import 'bootstrap/dist/css/bootstrap.css';
+@import "../styles/valiables.scss";
 </style>
