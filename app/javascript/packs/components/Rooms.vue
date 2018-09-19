da2f8a0897812b67ad0b53afcaead7c71f3ab210
Masayuki Higashino <msyk@hgsn.info>
2018-09-20 08:57:55 +0900

    Cleanup

--- app/javascript/packs/components/Rooms.vue
+++ app/javascript/packs/components/Rooms.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-  <nav-bar/>
+  <nav-bar />
   <div class="container">
-    <b-breadcrumb class="mt-3" :items="breadcrumb"/>
+    <b-breadcrumb class="mt-3" :items="breadcrumb" />
     <div class="tc-theme-light">
       <b-button variant="primary" v-b-modal.new_room>
         新規作成
@@ -36,7 +36,7 @@
       </b-alert>
       <b-form-group label="チャットルーム名" :feedback="feedbacks['name']" :state="states['name']">
         <b-input-group>
-          <b-form-input type="text" v-model="name"/>
+          <b-form-input type="text" v-model="name" />
         </b-input-group>
       </b-form-group>
     </b-modal>
@@ -139,7 +139,9 @@ export default {
       axios({
         url: '/api/rooms',
         method: 'post',
-        headers: { 'content-type': 'multipart/form-data' },
+        headers: {
+          'content-type': 'multipart/form-data'
+        },
         data: form,
         onUploadProgress: this.onUpload()
       }).then(function(response) {
@@ -160,32 +162,32 @@ export default {
 <style lang="scss">
 @import "../styles/valiables";
 body.dashboard {
-  background-color: lighten($tc-color-light, 30%);
+    background-color: lighten($tc-color-light, 30%);
 }
 div.tc-theme-light {
-  button.btn-primary {
-    border-radius: 0.25rem;
-    border: 0 none;
-    color: $tc-color-light;
-    background-color: $tc-color-dark;
-    &:hover {
-      color: darken($tc-color-light, 15%);
-      background-color: daraken($tc-color-dark, 30%);
+    button.btn-primary {
+        border-radius: 0.25rem;
+        border: 0 none;
+        color: $tc-color-light;
+        background-color: $tc-color-dark;
+        &:hover {
+            color: darken($tc-color-light, 15%);
+            background-color: daraken($tc-color-dark, 30%);
+        }
     }
-  }
-  // table {
-  //   td {
-  //     padding: 0;
-  //     a {
-  //       display: block;
-  //       width: 100%;
-  //       height: 100%;
-  //       padding: 12px;
-  //       &:hover {
-  //         text-decoration: none;
-  //       }
-  //     }
-  //   }
-  // }
+    // table {
+    //   td {
+    //     padding: 0;
+    //     a {
+    //       display: block;
+    //       width: 100%;
+    //       height: 100%;
+    //       padding: 12px;
+    //       &:hover {
+    //         text-decoration: none;
+    //       }
+    //     }
+    //   }
+    // }
 }
 </style>
