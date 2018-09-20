7ab5aec0169bbb48e159bc66f715070cd018acfa
Masayuki Higashino <msyk@hgsn.info>
2018-09-20 09:36:01 +0900

    Add the room view

--- app/javascript/packs/components/Room.vue
+++ app/javascript/packs/components/Room.vue
@@ -1,37 +1,51 @@
 <template>
 <div>
-  <nav-bar></nav-bar>
+  <nav-bar />
   <div class="container">
     <b-breadcrumb class="mt-3" :items="breadcrumb" />
-
-    <!-- <b-form-group horizontal label="管理者">
-      <b-form-input plaintext></b-form-input>
-    </b-form-group> -->
-
-    <h3>管理機能</h3>
-    <h4>認可</h4>
-    <h5>URL</h5>
-    <p>作成中</p>
-    <p>
-      <router-link :to="{name:'chat',params:{id:room.id}}" target="_blank">/chats/{{room.id}}</router-link>
-    </p>
-    <h5>QRコード発行</h5>
-    <p>作成中</p>
-    <table class="table table-hover">
-    </table>
-    <h5>トークン発行</h5>
-    <p>作成中</p>
-    <table class="table table-hover">
-    </table>
-    <h5>IPアドレス制限</h5>
-    <p>作成中</p>
-    <table class="table table-hover">
-    </table>
-    <h4>メッセージ</h4>
-    <h5>メッセージ一覧</h5>
-    <p>作成中</p>
-    <h5>CSVエクスポート</h5>
-    <p>作成中</p>
+    <h3 class="my-3">
+      <i class="fa fa-comments-o"></i>
+      <span>{{room.name}}</span>
+    </h3>
+    <b-card no-body>
+      <b-tabs ref="tabs" card>
+        <b-tab title="概要" active>
+          <h4>URL</h4>
+          開発中
+          <h4>QRコード</h4>
+          開発中
+        </b-tab>
+        <b-tab title="書き込み一覧">
+          開発中
+        </b-tab>
+        <b-tab title="アクセス制限">
+          <h4>暗証番号</h4>
+          <p>このチャットルームにアクセスするための暗証番号を管理します。セキュリティ対策のため、暗証番号は新規作成してから<strong>6時間</strong>だけ有効です。有効期限を過ぎると自動的に削除されます。このチャットルームへのログインを長期的に許可したい場合は、QRコードまたはURLによる認可方式を使用してください。
+          </p>
+          <b-button variant="primary" class="mb-3" v-b-modal.new_auth_code>
+            新規作成
+          </b-button>
+          <table class="table table-hover">
+            <thead class="thead-default">
+              <tr>
+                <th class="align-top">暗証番号</th>
+                <th class="align-top">有効期間の開始</th>
+                <th class="align-top">有効期間の終了</th>
+                <th class="align-top">利用回数</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="auth in codeAuthorizations">
+                <td class="align-top text-nowrap">{{auth.code}}</td>
+                <td class="align-top">{{auth.begin_at | moment}}</td>
+                <td class="align-top">{{auth.end_at | moment}}</td>
+                <td class="align-top text-right">{{auth.count}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </b-tab>
+      </b-tabs>
+    </b-card>
   </div>
 </div>
 </template>
@@ -39,6 +53,7 @@
 import NavBar from './NavBar.vue'
 import Vue from 'vue'
 import axios from 'axios'
+import moment from 'moment'
 export default {
   components: {
     NavBar
@@ -48,49 +63,79 @@ export default {
   },
   mounted() {
     this.getRoom()
+    this.getMessages()
   },
   data() {
     return {
       room: {
         id: '',
-        title: '',
+        name: '',
         updated_at: '',
         created_at: '',
+        authorizations: []
       },
+      messages: [],
       feedbacks: []
     }
   },
   computed: {
     breadcrumb() {
       return [{
-        text: 'チャットルーム',
-        href: '/rooms',
-        active: false
+        text: 'チャットルーム一覧',
+        href: '/rooms'
       }, {
-        text: this.room.title
+        text: this.room.name,
+        active: true
       }]
     },
-
+    codeAuthorizations() {
+      return this.room.authorizations.filter((e, i, a) => {
+        return e.factor === 'code'
+      })
+    }
+  },
+  filters: {
+    moment(date) {
+      moment.locale('ja');
+      return moment(date).format('llll');
+    }
   },
   methods: {
     getRoom() {
-      var vm = this;
-      axios
-        .get(`/api/v2/rooms/${vm.$route.params.id}`)
-        .then(function(response) {
-          vm.room = response.data;
+      let vm = this;
+      axios({
+        url: `/api/rooms/${vm.$route.params.id}`,
+        method: 'get'
+      }).then(function(response) {
+        vm.room = response.data;
+      })
+      .catch(function(error) {
+        // TODO error handling
+        console.log(response.error);
+      })
+    },
+    getMessages() {
+      let vm = this;
+      axios({
+        url: `/api/rooms/${vm.$route.params.id}/messages`,
+        method: 'get'
+      }).then(function(response) {
+        vm.messages = [];
+        response.data.forEach((message) => {
+          vm.messages.push(message)
         })
-        .catch(function(error) {
-          // TODO error handling
-          console.log(response.error);
+        Vue.nextTick(function() {
+          window.scrollTo(0, document.body.scrollHeight);
         })
+      })
+      .catch(function(error) {
+        // TODO error handling
+        console.log(response.error);
+      })
     }
   }
 }
 </script>
 <style lang="scss">
 @import "../styles/valiables";
-body {
-    background-color: lighten($tc-color-light, 30%);
-}
 </style>
