37396ad961d7d3a606583e53804972c0163ff6e5
Masayuki Higashino <msyk@hgsn.info>
2018-10-08 15:08:32 +0900

    Remove carrierwave.gem

--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,5 +1,4 @@
 class Sticker < ApplicationRecord
   belongs_to :group, class_name: 'StickerGroup', foreign_key: 'sticker_group_id'
   has_many :messages
-  mount_uploader :image, StickerUploader
 end
